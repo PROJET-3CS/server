f485fe5c32c939a4d66492a8c883b4917a7ee030
Neoxs <m.abdelkaderkharoubi@esi-sba.dz>
2021-08-13 20:46:44 +0100

    ♻️ Refactor code

--- src/modules/BDD/database.provider.ts
+++ src/modules/BDD/database.provider.ts
@@ -31,7 +31,6 @@ export const databaseProvider = {
 
     const sequelize = new Sequelize(config);
     sequelize.addModels([User]);
-    sequelize.sync({force : true})
     return sequelize;
   },
 };
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,64 +1,75 @@
-import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
-import { User } from '../users/user.entity';
-import * as jwt from 'jsonwebtoken';
+import { Inject, Injectable, UnauthorizedException } from "@nestjs/common";
+import { User } from "../users/user.entity";
+import * as jwt from "jsonwebtoken";
 const { Op } = require("sequelize");
 import * as dotenv from "dotenv";
 dotenv.config();
 
 @Injectable()
-export class AuthService  {
-    constructor(
-        @Inject('UserRepository') private readonly userRepository: typeof User,
-        @Inject('SequelizeInstance') private readonly sequelizeInstance,
+export class AuthService {
+  constructor(
+    @Inject("UserRepository") private readonly userRepository: typeof User,
+    @Inject("SequelizeInstance") private readonly sequelizeInstance
+  ) {}
 
-    ) {}
+  private _options: any = {
+    algorithm: "HS256",
+    expiresIn: "1 days",
+    jwtid: process.env.JWT_ID || "",
+  };
 
-    private _options: any = {
-        algorithm: 'HS256',
-        expiresIn: '1 days',
-        jwtid: process.env.JWT_ID || ''
-    };
+  public async login(loginObject: any): Promise<object> {
+    const { email, password } = loginObject;
 
+    const user = await this.userRepository.findOne({
+      where: {
+        [Op.and]: [{ email: email }, { password: password }],
+      },
+    });
 
-    public async login(loginObject: any): Promise<object> {
-        const { email , password } = loginObject;
-        console.log(loginObject)
-    
-        const user = await this.userRepository.findOne({               
-            where: {
-                [Op.and]: [
-                    { email: email },
-                    { password: password }
-                  ]
-              }
-        } );
-                
-        if(!user){
-            throw new  UnauthorizedException();  
-        }else {
-        
-        const payload = {
-            id: user.id,
-            email: user.email,
-            password: user.password
-        };
+    if (!user) {
+      throw new UnauthorizedException();
+    } else {
+      const payload = {
+        id: user.id,
+        email: user.email,
+        password: user.password,
+      };
 
-        var token = jwt.sign(payload, process.env.JWT_KEY || 'JWT_KEY', this._options)
-        
-        user.password = undefined;
+      var token = jwt.sign(
+        payload,
+        process.env.JWT_KEY || "JWT_KEY",
+        this._options
+      );
 
-        return {token,user}
-        }
+      user.password = undefined;
+
+      return { token, user };
+    }
+  }
+  public async verify_token(token: string): Promise<Object> {
+    try {
+      const isValid: Object = jwt.verify(
+        token,
+        process.env.JWT_KEY || "JWT_KEY"
+      );
+
+      let user;
+      if (isValid) {
+        const decoded: any = jwt.decode(token);
+        user = await this.userRepository.findOne({
+          where: {
+            [Op.and]: [
+              { email: decoded.email },
+              { password: decoded.password },
+            ],
+          },
+        });
       }
-      public async verify_token(token: string): Promise<Boolean> {
-          try {
-            const isValid: Object = jwt.verify(token, process.env.JWT_KEY||'JWT_KEY');
 
-            return isValid ? true : false;
-              
-          } catch (error) {
-            return false
-          }
-         
-      }
+      return { user, isValid: isValid && user? true : false };
+    } catch (error) {
+      return { user: "", isValid: false };
+    }
+  }
 }
