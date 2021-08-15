ccb4a8c24b1874e607a310950429e87ac9913ae5
AYMEN <a.zitouni@esi-sba.dz>
2021-08-15 03:38:38 +0100

    :wheelchair: Improve medical folder model

--- src/modules/users/models/medical-folder.entity.ts
+++ src/modules/users/models/medical-folder.entity.ts
@@ -5,7 +5,10 @@ import {
   DataType,
   Unique,
   IsEmail,
+  ForeignKey,
 } from "sequelize-typescript";
+import { User } from "./user.entity";
+import { Blood } from "./../../../shared/enums/blood.enum";
 
 @Table
 export class MedicalFolder extends Model {
@@ -16,4 +19,79 @@ export class MedicalFolder extends Model {
     primaryKey: true,
   })
   public id: number;
+
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.UUID,
+    field: "user_id",
+  })
+  userId: string;
+
+  //Personal infos
+  @Column
+  tall: number;
+
+  @Column
+  weight: number;
+
+  @Column({
+    type: DataType.ENUM(
+      Blood.APositif,
+      Blood.ANegatif,
+      Blood.BPositif,
+      Blood.BNegatif,
+      Blood.ABPositif,
+      Blood.ABNegatif,
+      Blood.OPositif,
+      Blood.ONegatif
+    ),
+  })
+  blood: Blood;
+
+  @Column
+  imc: string;
+
+  //Personal History
+  @Column
+  smoker: boolean;
+
+  @Column({ field: "nbr_cigarettes" })
+  nbrCigarettes: number;
+
+  @Column
+  chewer: boolean;
+
+  @Column
+  alcoholic: boolean;
+
+  @Column({ field: "ex_smoker" })
+  exSmoker: boolean;
+
+  @Column({ type: DataType.ARRAY(DataType.STRING) })
+  medicaments: string[];
+
+  @Column
+  other: string;
+
+  //Surgical Medicinal History
+  @Column({
+    field: "congenital_infections",
+    type: DataType.ARRAY(DataType.STRING),
+  })
+  congenitalInfections: string[];
+
+  @Column({ field: "general_illnesses", type: DataType.ARRAY(DataType.STRING) })
+  generalIllnesses: string[];
+
+  @Column({
+    field: "surgical_interventions",
+    type: DataType.ARRAY(DataType.STRING),
+  })
+  surgicalInterventions: string[];
+
+  @Column({
+    field: "allergic_reactions",
+    type: DataType.ARRAY(DataType.STRING),
+  })
+  allergicReactions: string[];
 }
---
+++ src/shared/enums/blood.enum.ts
@@ -0,0 +1,10 @@
+export enum Blood {
+  APositif = "A+",
+  ANegatif = "A-",
+  BPositif = "B+",
+  BNegatif = "B-",
+  ABPositif = "AB+",
+  ABNegatif = "AB-",
+  OPositif = "O+",
+  ONegatif = "O-",
+}
